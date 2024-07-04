8266cb70d16f392a69419a2eb4228024147237d8
amajad <ansariamajad99@gmail.com>
2024-07-04 17:30:28 +0530

    add nav , autoloader ,traits and classes

--- functions.php
+++ functions.php
@@ -6,19 +6,34 @@
  * @package Tech Theme
  */
 
+
+if(!defined('TECH_DIR_PATH')){
+    define('TECH_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+
+require_once TECH_DIR_PATH.'./inc/helpers/autoloader.php';
+
+function aquila_get_theme_instance() {
+      \TECH_THEME\Inc\TECH_THEME::get_instance();
+}
 // echo '<pre>';
-// print_r(filemtime(get_template_directory().'/style.css'));
+// print_r(\TECH_THEME\Inc\TECH_THEME::get_instance());
 // wp_die();
+aquila_get_theme_instance();
+
+
+
 function techtheme_enqueue_scripts()
 {
     //register styles
     wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'));
-    wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap-grid.min.css', [], false);
+    wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false);
 
 
     //register scripts
     wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
-    wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.js', [ 'jquery' ], false, true);
+    wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', [ 'jquery' ], false, true);
     
     //enqueue styles
     wp_enqueue_style('style-css');
